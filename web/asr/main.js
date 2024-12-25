bcc21c3c8ac1f02bbbe7cc83692b1dc4b144c385
lipku <lipku@foxmail.com>
2024-12-25 18:10:32 +0800

    fix asr sessionid

--- web/asr/main.js
+++ web/asr/main.js
@@ -348,7 +348,7 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 async function is_speaking() {
 	const response = await fetch('/is_speaking', {
 		body: JSON.stringify({
-			sessionid: 0,
+			sessionid: parseInt(parent.document.getElementById('sessionid').value),
 		}),
 		headers: {
 			'Content-Type': 'application/json'
@@ -396,6 +396,7 @@ function getJsonMessage( jsonMsg ) {
             body: JSON.stringify({
                 text: rectxt.replace(/ +/g,""),
                 type: 'chat',
+				sessionid:parseInt(parent.document.getElementById('sessionid').value),
             }),
             headers: {
                 'Content-Type': 'application/json'
